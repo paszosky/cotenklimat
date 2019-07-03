93738dd6f8c5378be2346d3296091f50138cb3b5
pasz <pavel.kowalczyk@gmail.com>
2019-07-03 10:38:16 +0200

    added content

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,18 @@
 import moment from 'moment';
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+// hamburger menu
+const navBtn = document.querySelector('.navigation__btn-js');
+// console.log(navBtn);
+
+navBtn.addEventListener('click', () => {
+    const navigation = document.querySelector('.navigation__list--js');
+    navigation.classList.toggle('navigation__list--visible');
+    if (navigation.classList.contains('navigation__list--visible')) {
+        navBtn.innerHTML = "X";
+    } else {
+        navBtn.innerHTML = "☰";
+    }
+});
+
